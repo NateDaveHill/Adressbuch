2b8f094bbd98e0ac38203bf98c66003fdc6d7c79
Nathan David Hill <hill@software.tirol>
2022-09-28 10:11:33 +0200

    1. Added enum and Methods

---
+++ Adressdaten/Adressbuch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adressverwaltung
+{
+    public class Adressbuch
+    {
+        public enum AdressType
+        {
+            Kunden,
+            Lieferanten,
+            Privat
+        }
+
+        public Adressbuch(Adressverwaltung.Adressbuch.AdressType type)
+        {
+            AddAdress();
+            DelAdress();
+            CountAdress();
+        }
+
+        private void CountAdress()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DelAdress()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddAdress()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
