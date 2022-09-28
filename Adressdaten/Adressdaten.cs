282e38e28ee5a6b9ff2ecb723446b0b105d0f4e3
Nathan David Hill <hill@software.tirol>
2022-09-28 10:00:30 +0200

    0. commit for all

---
+++ Adressdaten/Adressdaten.cs
@@ -0,0 +1,12 @@
+﻿namespace Adressdaten
+{
+    public class Adressdaten
+    {
+        public string Firma { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public string Adresse { get; set; }
+        public int PLZ { get; set; }
+        public string Ort { get; set; }
+    }
+}
