36611ab4698050fb3bd5bdb7bb858ef823acb813
Nathan David Hill <hill@software.tirol>
2022-10-04 08:27:59 +0200

    test remote commit

---
+++ Adressbuch/Adressdaten/Adressdaten.cs
@@ -0,0 +1,13 @@
+﻿namespace Adressverwaltung
+{
+    public class Adressdaten
+    {
+        public string? Type { get; set; }
+        public string? Firma { get; set; }
+        public string? Vorname { get; set; }
+        public string? Nachname { get; set; }
+        public string? Adresse { get; set; }
+        public string? PLZ { get; set; }
+        public string? Ort { get; set; }
+    }
+}
---
+++ Adressbuch/Adressdaten/Adressverwaltung.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adressverwaltung
+{
+    public class Adressbuch
+    {
+        //Konstruktor
+        public Adressbuch(Type adressType)
+        {
+        }
+
+        //Enum für Combobox
+        public enum AdressTypeEnum
+        {
+            Kunden,
+            Lieferanten,
+            Privat
+        }
+    }
+}
